43f3da8dbb5721be859becd0bf1563c34b86edff
Ping-Jung <79309902+Ping-Jung@users.noreply.github.com>
2022-03-15 16:51:53 +0800

    test1

--- kachikawawa/Main.java
+++ kachikawawa/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) {
       long lastime=System.nanoTime();
 	  game game=new game("SLimevancoff",600,600);
+	  print("yes")
 	  game.start();
 	  Musicontrol player = new Musicontrol();
 	  player.play();
